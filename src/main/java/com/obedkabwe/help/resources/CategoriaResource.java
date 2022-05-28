81d10d6078aa3857ccb0edd4f1d19a13f22d18b6
obedkabwe <kabweobed@gmail.com>
2022-05-28 18:16:30 -0500

    our rest is working

---
+++ src/main/java/com/obedkabwe/help/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.obedkabwe.help.resources;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listen () {
+		return "its'working";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
