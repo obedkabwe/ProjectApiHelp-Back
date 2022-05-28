23f81bd091be2318b5e0bcc7ec5177e7271277c0
obedkabwe <kabweobed@gmail.com>
2022-05-27 21:51:40 -0500

    Project Created

---
+++ src/main/java/com/obedkabwe/help/CursoapiApplication.java
@@ -0,0 +1,13 @@
+package com.obedkabwe.help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obedkabwe/help/CursoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obedkabwe.help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
