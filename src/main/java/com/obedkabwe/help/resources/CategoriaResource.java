c8f4ba7c600a651dc3f1e5c8e34082493cc1dc8a
obedkabwe <kabweobed@gmail.com>
2022-05-28 19:04:57 -0500

    testing the first class of domain-categoria

--- src/main/java/com/obedkabwe/help/resources/CategoriaResource.java
+++ src/main/java/com/obedkabwe/help/resources/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.obedkabwe.help.resources;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.obedkabwe.help.domain.Categoria;
 
 
 @RestController
@@ -9,8 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listen () {
-		return "its'working";
+	public List<Categoria> listen () {
+		
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
+		
 	}
 	
 	
