58c860fbc77a27069e2877f9ef63998d3a7275dc
obedkabwe <kabweobed@gmail.com>
2022-06-02 21:11:16 -0500

    Many to Many with association with product

--- src/main/java/com/obedkabwe/help/CursoapiApplication.java
+++ src/main/java/com/obedkabwe/help/CursoapiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.obedkabwe.help.domain.Categoria;
+import com.obedkabwe.help.domain.Produto;
 import com.obedkabwe.help.repositories.CategoriaRepository;
+import com.obedkabwe.help.repositories.ProdutoRepository;
 
 
 @SpringBootApplication
@@ -15,7 +17,9 @@ public class CursoapiApplication implements CommandLineRunner   {
 
 
 	@Autowired
-	CategoriaRepository repository;
+	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoapiApplication.class, args);
@@ -27,7 +31,35 @@ public class CursoapiApplication implements CommandLineRunner   {
 		
 		Categoria cat1 = new Categoria("informatica");
 		Categoria cat2 = new Categoria("escritorio"); 
-		repository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null,"impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+		
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
--- src/main/java/com/obedkabwe/help/domain/Categoria.java
+++ src/main/java/com/obedkabwe/help/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.obedkabwe.help.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	
 	public Categoria() {
@@ -70,6 +76,20 @@ public class Categoria implements Serializable{
 			return false;
 		Categoria other = (Categoria) obj;
 		return id == other.id;
+	}
+
+
+
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	
